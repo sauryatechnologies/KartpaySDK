8171c1a3fe756c400468615e9141d252c2de62fe
saurabh <saurabh.iosdevlpr@gmail.com>
2019-03-28 12:50:25 +0530

    Initial Commit

---
+++ KartpaySDK/KartpaySDK.h
@@ -0,0 +1,19 @@
+//
+//  KartpaySDK.h
+//  KartpaySDK
+//
+//  Created by iMAC on 28/03/19.
+//  Copyright © 2019 Saurabh.Chaudhari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KartpaySDK.
+FOUNDATION_EXPORT double KartpaySDKVersionNumber;
+
+//! Project version string for KartpaySDK.
+FOUNDATION_EXPORT const unsigned char KartpaySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KartpaySDK/PublicHeader.h>
+
+
